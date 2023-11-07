28458490a35a5e64afc65b4c4c52d29038664299
Mohamed-Trabelsi <74937530+Mohamed-Trabelsi@users.noreply.github.com>
2023-11-07 11:30:06 +0100

    Update MockitoTest.java

--- src/test/java/tn/esprit/rh/achat/MockitoTest.java
+++ src/test/java/tn/esprit/rh/achat/MockitoTest.java
@@ -1,50 +1,94 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
 
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
+package tn.esprit.rh.achat;
 
-import org.junit.jupiter.api.Test;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import tn.esprit.rh.achat.controllers.StockRestController;
+import tn.esprit.rh.achat.entities.Stock;
+import tn.esprit.rh.achat.repositories.StockRepository;
+import tn.esprit.rh.achat.services.StockServiceImpl;
 
-class ProduitTest {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+import static com.fasterxml.jackson.databind.jsonFormatVisitors.JsonValueFormat.UUID;
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+@RunWith( SpringRunner.class)
+@ContextConfiguration(classes = {StockServiceImpl.class})
+public class MockitoTest {
+
+    private StockServiceImpl service;
+    private StockRepository repository;
+//    private MockMvc mockMvc;
+//    ObjectMapper objectMapper = new ObjectMapper();
+//    ObjectWriter objectWriter = objectMapper.writer();
+//@Mock
+// private StockRepository stockRepository;
+//@InjectMocks
+//    private StockRestController stockRestController;
+//
+//    long id1 = java.util.UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
+//    long id2 = java.util.UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
+//    long id3 = java.util.UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
+//    Stock RECORD_1 = new Stock(id1,"alimentaire",50,5);
+//    Stock RECORD_2 = new Stock(id2,"électro",100,20);
+//    Stock RECORD_3 = new Stock(id3,"vestimentaire",150,10);
+//@Before
+//    public void setUp(){
+//    MockitoAnnotations.initMocks(this);
+//    this.mockMvc = MockMvcBuilders.standaloneSetup(stockRestController).build();
+//
+//   }
+//@Test
+////    public void getAllRecords_success()throws Exception{
+//
+//    List<Stock> records = new ArrayList<>(Arrays.asList(RECORD_1,RECORD_2,RECORD_3));
+//    Mockito.when(stockRepository.findAll()).thenReturn(records);
+//    mockMvc.perform(MockMvcRequestBuilders
+//            .get("/retrieve-all-stocks")
+//            .contentType(MediaType.APPLICATION_JSON))
+//            .andExpect(status().isOk())
+//            .andExpect(MockMvcResultMatchers.jsonPath("$",hasSize(3)))
+//            .andExpect((ResultMatcher) jsonPath("$[2].libelleStock", is("alimentaire")));
+//}
 
     @Test
-    void testGettersAndSetters() {
-        // Create a mock Stock
-        Stock stockMock = mock(Stock.class);
-        
-        // Create a mock CategorieProduit
-        CategorieProduit categorieProduitMock = mock(CategorieProduit.class);
+    public void getStockTest(){
+        System.out.println(" get test stock");
+        long id = java.util.UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
+        long id2 = java.util.UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
 
-        // Create a Set of DetailFacture (mocked)
-        Set<DetailFacture> detailFactureSetMock = new HashSet<>();
+        repository = mock(StockRepository.class);
+        service = new StockServiceImpl(repository);
 
-        // Create a Produit instance
-        Produit produit = new Produit();
-        produit.setIdProduit(1L);
-        produit.setCodeProduit("ABC123");
-        produit.setLibelleProduit("Test Produit");
-        produit.setPrix(25.0f);
-        produit.setDateCreation(new Date());
-        produit.setDateDerniereModification(new Date());
-        produit.setStock(stockMock);
-        produit.setDetailFacture(detailFactureSetMock);
-        produit.setCategorieProduit(categorieProduitMock);
+        List<Stock> stockList = new ArrayList<>();
+        stockList.add(new Stock(id,"alimentaire",100,5));
+        stockList.add(new Stock(id2,"vest",50,10));
+        when(repository.findAll()).thenReturn(stockList);
 
-        // Verify the getters
-        assertEquals(1L, produit.getIdProduit());
-        assertEquals("ABC123", produit.getCodeProduit());
-        assertEquals("Test Produit", produit.getLibelleProduit());
-        assertEquals(25.0f, produit.getPrix());
-        assertEquals(stockMock, produit.getStock());
-        assertEquals(detailFactureSetMock, produit.getDetailFacture());
-        assertEquals(categorieProduitMock, produit.getCategorieProduit());
-
-        // You can add more assertions as needed
-
-        // Verify interactions with mocked objects
-        verify(stockMock, times(1)).addProduit(produit);
-        verify(categorieProduitMock, times(1)).addProduit(produit);
     }
+
 }
