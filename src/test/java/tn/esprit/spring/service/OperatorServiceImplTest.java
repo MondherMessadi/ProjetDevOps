fb755215a0ee457537e6d567f8af63957f74cafe
Hassen <98349666+HassenBenadel@users.noreply.github.com>
2023-11-08 19:54:40 +0100

    OperatorServiceImplTest.java

--- src/test/java/tn/esprit/spring/service/OperatorServiceImplTest.java
+++ src/test/java/tn/esprit/spring/service/OperatorServiceImplTest.java
@@ -1,30 +1,55 @@
 package tn.esprit.spring.service;
 
 
-import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import tn.esprit.rh.achat.entities.Operateur;
-import tn.esprit.rh.achat.services.IOperateurService;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import java.util.List;
-
-@SpringBootTest
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class OperatorServiceImplTest {
+    private Operateur operateur;
 
-    @Autowired
-    IOperateurService ios;
 
-    @Test
-    @Order(1)
-    public void testRetreiveAllOp(){
+    @BeforeEach
+    public void setUp() {
+        operateur = new Operateur();
+    }
+
+   /* @Test
+    public void testRetrieveAllOp() {
         List<Operateur> ops = ios.retrieveAllOperateurs();
         Assertions.assertEquals(0, ops.size());
+    }*/
+
+    @Test
+    public void testOperateur() {
+        operateur.setIdOperateur(10L);
+        operateur.setNom("Benadel");
+        operateur.setPrenom("MohamedHassen");
+
+        assertEquals(10L, operateur.getIdOperateur());
+        assertEquals("Benadel", operateur.getNom());
+        assertEquals("MohamedHassen", operateur.getPrenom());
+    }
+
+    @Test
+    public void testOperateurNotNull() {
+        operateur.setIdOperateur(10L);
+        operateur.setNom("Benadel");
+        operateur.setPrenom("MohamedHassen");
+
+        assertNotNull(operateur.getIdOperateur());
+        assertNotNull(operateur.getNom());
+        assertNotNull(operateur.getPrenom());
+
+
     }
 
 
