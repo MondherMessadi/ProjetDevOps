14e2e1edccd24d0934758ee3c0ceb799cd4a19be
Mohamed-Trabelsi <mohamed.trabelsi@artibedded.com>
2023-11-08 19:56:33 +0100

    Junit

---
+++ src/test/java/tn/esprit/spring/service/FactureServiceImplTest.java
@@ -0,0 +1,66 @@
+package tn.esprit.spring.service;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import tn.esprit.rh.achat.entities.Fournisseur;
+import tn.esprit.rh.achat.entities.DetailFacture;
+import tn.esprit.rh.achat.entities.Facture;
+import java.util.*;
+import static org.junit.Assert.assertSame;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class FactureServiceImplTest {
+
+    private Facture facture;
+
+    @BeforeEach
+    public void setUp() {
+        facture = new Facture();
+    }
+
+    @Test
+    public void testFactureFields() {
+
+        facture.setIdFacture(1L);
+        facture.setMontantRemise(100.0f);
+        facture.setMontantFacture(500.0f);
+
+
+        assertEquals(1L, facture.getIdFacture());
+        assertEquals(100.0f, facture.getMontantRemise());
+        assertEquals(500.0f, facture.getMontantFacture());
+    }
+
+    @Test
+    public void testFactureDateFields() {
+
+        facture.setDateCreationFacture(new Date());
+        facture.setDateDerniereModificationFacture(new Date());
+
+
+        assertNotNull(facture.getDateCreationFacture());
+        assertNotNull(facture.getDateDerniereModificationFacture());
+    }
+
+    @Test
+    public void testFactureRelationships() {
+
+        Set<DetailFacture> detailsFacture = new HashSet<>();
+        Fournisseur fournisseur = new Fournisseur();
+
+
+        facture.setDetailsFacture(detailsFacture);
+        facture.setFournisseur(fournisseur);
+
+
+        assertSame(detailsFacture, facture.getDetailsFacture());
+        assertSame(fournisseur, facture.getFournisseur());
+    }
+}
