439663189f5d4cf9eca50abb99082fb8916f3bb8
Mohamed-Trabelsi <mohamed.trabelsi@artibedded.com>
2023-11-08 21:25:35 +0100

    Junit

--- src/test/java/tn/esprit/spring/service/StockServiceImplTest.java
+++ src/test/java/tn/esprit/spring/service/StockServiceImplTest.java
@@ -1,8 +1,14 @@
+package tn.esprit.spring.service;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import tn.esprit.rh.achat.entities.Stock;
+import tn.esprit.rh.achat.repositories.StockRepository;
+import tn.esprit.rh.achat.services.StockServiceImpl;
+
 import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
